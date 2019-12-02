15d55362cc1574197776768dc588b510fc884006
Mate Marschalko <webondevices@gmail.com>
2019-12-02 15:02:06 +0000

    feat: add webpack, typescript, react

---
+++ src/components/App.d.ts
@@ -0,0 +1,6 @@
+import * as React from "react";
+import PageInterface from "./PageInterface";
+declare class App extends React.Component<PageInterface, {}> {
+    render(): JSX.Element;
+}
+export default App;
